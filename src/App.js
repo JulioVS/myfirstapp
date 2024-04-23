c64d6c2be6ea7fcaec4f56132b0b2d1d686bb9d0
JulioVS <julioer@me.com>
2024-04-23 13:23:27 -0400

    Added name and date display

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ function App(props) {
   
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Hello, world! It's Julio here from Coursera</h1>
+        <h2>The date now is {currDate.toLocaleDateString()}.</h2>
         <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
       </div>
     );
